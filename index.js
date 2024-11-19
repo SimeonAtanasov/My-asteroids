a99a03137849f5fe3f290f0514c38b9921130703
Simeon Atanasov <sim.atanasov@gmail.com>
2024-11-19 11:34:49 +0200

    Update index.js
    Hit asteroids

--- index.js
+++ index.js
@@ -183,7 +183,20 @@ window.setInterval(() => {
   )
   console.log(asteroids)
 }, 3000 )// 3000 milliseconds
- 
+
+function circleCollision(circle1, circle2){
+  const xDifference = circle2.position.x - circle1.position.x
+  const yDifference = circle2.position.y - circle1.position.y
+
+  const distance = Math.sqrt(
+    xDifference * xDifference + yDifference * yDifference
+  )
+
+  if (distance <= circle1.radius + circle2.radius){
+    return true
+  }
+  return false
+}
 
 function animate() {
   window.requestAnimationFrame(animate)
@@ -213,6 +226,9 @@ function animate() {
     const asteroid = asteroids[i]
     asteroid.update()
 
+
+
+
     // garbage collection for asteroids
     if (
       asteroid.position.x + asteroid.radius < 0 ||
@@ -221,6 +237,15 @@ function animate() {
       asteroid.position.y + asteroid.radius < 0
     ) {
       asteroids.splice(i, 1)
+    }
+    // projectiles
+    for (let j = projectiles.length - 1; j >= 0; j--) {
+      const projectile = projectiles[j]
+
+      if(circleCollision(asteroid, projectile)){
+        asteroids.splice(i, 1)
+        projectiles.splice(j, 1)
+      }
     }
   }
 
